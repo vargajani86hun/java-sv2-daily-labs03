a4179e623c64fd46e86851a3b23aaa02812cd72a
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-26 15:56:59 +0200

    Mathematics class fixed.

--- day02/src/main/java/day02/Mathematics.java
+++ day02/src/main/java/day02/Mathematics.java
@@ -2,7 +2,10 @@ package day02;
 
 public class Mathematics {
     public boolean isPrime(int number) {
-        for (int i = 2; i < (number / 2); i++) {
+        if (number == 0 || number == 1) {
+            return false;
+        }
+        for (int i = 2; i <= (number / 2); i++) {
             if ((number % i) == 0) {
                 return false;
             }
