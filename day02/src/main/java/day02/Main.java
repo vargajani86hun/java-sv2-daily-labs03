6aefac5c54627ac8ba423fb13c0302496be53985
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-26 15:45:48 +0200

    Main class for testing is added.

---
+++ day02/src/main/java/day02/Main.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Main {
+    public static void main(String[] args) {
+        Mathematics math = new Mathematics();
+        System.out.println(math.isPrime(10));
+        System.out.println(math.isPrime(7));
+        System.out.println(math.isPrime(121));
+        System.out.println(math.isPrime(101));
+    }
+}
